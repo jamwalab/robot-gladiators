b66e6e28570f7994cf64edb693e68553c8f3e917
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-04-28 12:08:40 -0400

    console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! Javascript is running!")
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot name?");
+
+console.log(playerName);
+
+console.log("Thiasaddasdasfasfa");
+
+console.log(10+10);
+
+console.log("Our Robot name is " + playerName);
+
+// fight();
